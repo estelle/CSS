4be2b00f0d29cbae8644e3907963739555b57fc0
Estelle Weyl <estelle@weyl.org>
2019-07-03 23:24:56 -0700

    addition of links section, separate appearance from color

---
+++ houdini/example2/image.js
@@ -0,0 +1,14 @@
+registerPaint('boxbg', class {
+
+  // Whether Alpha is allowed - This is set to true by default, if it is set to false all colours used on the canvas will have full opacity, or alpha of 1.0
+  static get contextOptions() { return {alpha: true}; }
+
+  paint(ctx, size) {
+    // ctx -> drawing context
+
+		ctx.fillStyle = 'hsla(55, 90%, 60%, 1.0)';
+		ctx.fillRect(0, size.height/3, size.width*0.4, size.height*0.6);
+		console.dir(size);
+
+  }
+});
